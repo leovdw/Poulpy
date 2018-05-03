07f85ad63d8ee1b362fadd2a902504df72c62ed5
Benoit Lemaire <benoit.lemaire@hetic.net>
2018-05-04 01:40:55 +0200

    fix

--- js/home.js
+++ js/home.js
@@ -9,17 +9,19 @@ function loader() {
 
   let loaderValue;
 
-  enseigner.addEventListener("click", function() {
-    loaderValue = "enseigner";
-    moveLoader();
-    tabs(loaderValue);
-  });
+  if (enseigner || former) {
+    enseigner.addEventListener("click", function() {
+      loaderValue = "enseigner";
+      moveLoader();
+      tabs(loaderValue);
+    });
 
-  former.addEventListener("click", function() {
-    loaderValue = "former";
-    moveLoader();
-    tabs(loaderValue);
-  });
+    former.addEventListener("click", function() {
+      loaderValue = "former";
+      moveLoader();
+      tabs(loaderValue);
+    });
+  }
 }
 
 function moveLoader() {
@@ -38,19 +40,21 @@ function moveLoader() {
 // Change flag
 const flag = document.querySelector(".right-link > img");
 
-flag.addEventListener('click', function () {
-  if (flag.className == 'mg') {
-    flag.src = "img/fr.png";
+if (flag) {
+  flag.addEventListener("click", function() {
+    if (flag.className == "mg") {
+      flag.src = "img/fr.png";
 
-    flag.classList.remove("mg");
-    flag.classList.add('fr')
-  } else {
-    flag.src = "img/mg.png";
+      flag.classList.remove("mg");
+      flag.classList.add("fr");
+    } else {
+      flag.src = "img/mg.png";
 
-    flag.classList.remove("fr");
-    flag.classList.add("mg");
-  }
-})
+      flag.classList.remove("fr");
+      flag.classList.add("mg");
+    }
+  });
+}
 
 // TABS
 function tabs (value) {
@@ -75,33 +79,36 @@ function tabs (value) {
     }
   }
 
-  leftLink.addEventListener('click', function () {
-    leftLink.classList.remove('is-active');
-    leftLink.classList.remove('is-inactive');
-    rightLink.classList.remove("is-inactive");
-    rightLink.classList.remove("is-inactive");
+  if (leftLink || rightLink) {
+    leftLink.addEventListener("click", function() {
+      leftLink.classList.remove("is-active");
+      leftLink.classList.remove("is-inactive");
+      rightLink.classList.remove("is-inactive");
+      rightLink.classList.remove("is-inactive");
 
-    leftLink.classList.add("is-active");
-    rightLink.classList.add("is-inactive");
-    step.dataset.step = 1;
-    breadcrumb();
+      leftLink.classList.add("is-active");
+      rightLink.classList.add("is-inactive");
+      step.dataset.step = 1;
+      breadcrumb();
 
-    document.querySelector('.progress').style.display = 'none';
-  });
+      document.querySelector(".progress").style.display = "none";
+    });
 
-  rightLink.addEventListener('click', function () {
-    leftLink.classList.remove("is-active");
-    leftLink.classList.remove("is-inactive");
-    rightLink.classList.remove("is-inactive");
-    rightLink.classList.remove("is-inactive");
+    rightLink.addEventListener("click", function() {
+      leftLink.classList.remove("is-active");
+      leftLink.classList.remove("is-inactive");
+      rightLink.classList.remove("is-inactive");
+      rightLink.classList.remove("is-inactive");
 
-    leftLink.classList.add("is-inactive");
-    rightLink.classList.add("is-active");
-    step.dataset.step = 2;
-    breadcrumb();
+      leftLink.classList.add("is-inactive");
+      rightLink.classList.add("is-active");
+      step.dataset.step = 2;
+      breadcrumb();
 
-    document.querySelector(".progress").style.display = "block";
-  })
+      document.querySelector(".progress").style.display = "block";
+    });
+  }
+
 }
 
 // Breadcrumb
@@ -110,54 +117,56 @@ function breadcrumb () {
   const bullets = document.querySelectorAll('.bullet');
   const links = document.querySelectorAll('.bullet a');
 
-  if (step.dataset.step == 1) {
-    step.style.width = "0" + "%";
+  if (step) {
+    if (step.dataset.step == 1) {
+      step.style.width = "0" + "%";
 
-    bullets[0].style.backgroundColor = "#2E95A7";
-    bullets[1].style.backgroundColor = "#D5D5D5";
-    links[0].style.color = "#2E95A7";
-    links[0].style.opacity = 1;
+      bullets[0].style.backgroundColor = "#2E95A7";
+      bullets[1].style.backgroundColor = "#D5D5D5";
+      links[0].style.color = "#2E95A7";
+      links[0].style.opacity = 1;
 
-    links[1].style.color = "#D5D5D5";
-    links[1].style.opacity = 0.5;
+      links[1].style.color = "#D5D5D5";
+      links[1].style.opacity = 0.5;
+    } else if (step.dataset.step == 2) {
+      step.style.width = "33" + "%";
 
-  } else if (step.dataset.step == 2) {
-    step.style.width = "33" + "%";
+      bullets[0].style.backgroundColor = "#2E95A7";
+      links[0].style.color = "#2E95A7";
+      links[0].style.opacity = 1;
 
-    bullets[0].style.backgroundColor = "#2E95A7";
-    links[0].style.color = "#2E95A7";
-    links[0].style.opacity = 1;
+      bullets[1].style.backgroundColor = "#2E95A7";
+      links[1].style.color = "#2E95A7";
+      links[1].style.opacity = 1;
+    } else if (step.dataset.step == 3) {
+      links[0].style.color = "#2E95A7";
+      links[0].style.opacity = 1;
 
-    bullets[1].style.backgroundColor = "#2E95A7";
-    links[1].style.color = "#2E95A7";
-    links[1].style.opacity = 1;
-  } else if (step.dataset.step == 3) {
-    links[0].style.color = "#2E95A7";
-    links[0].style.opacity = 1;
+      bullets[1].style.backgroundColor = "#2E95A7";
+      links[1].style.color = "#2E95A7";
+      links[1].style.opacity = 1;
 
-    bullets[1].style.backgroundColor = "#2E95A7";
-    links[1].style.color = "#2E95A7";
-    links[1].style.opacity = 1;
+      step.style.width = "66" + "%";
+      bullets[2].style.backgroundColor = "#2E95A7";
+      links[2].style.color = "#2E95A7";
+      links[2].style.opacity = 1;
+    } else if (step.dataset.step == 4) {
+      links[0].style.color = "#2E95A7";
+      links[0].style.opacity = 1;
 
-    step.style.width = "66" + "%";
-    bullets[2].style.backgroundColor = "#2E95A7";
-    links[2].style.color = "#2E95A7";
-    links[2].style.opacity = 1;
-  } else if (step.dataset.step == 4) {
-    links[0].style.color = "#2E95A7";
-    links[0].style.opacity = 1;
+      step.style.width = "100" + "%";
+      bullets[1].style.backgroundColor = "#2E95A7";
+      links[1].style.color = "#2E95A7";
+      links[1].style.opacity = 1;
 
-    step.style.width = "100" + "%";
-    bullets[1].style.backgroundColor = "#2E95A7";
-    links[1].style.color = "#2E95A7";
-    links[1].style.opacity = 1;
+      bullets[2].style.backgroundColor = "#2E95A7";
+      links[2].style.color = "#2E95A7";
+      links[2].style.opacity = 1;
 
-    bullets[2].style.backgroundColor = "#2E95A7";
-    links[2].style.color = "#2E95A7";
-    links[2].style.opacity = 1;
-
-    bullets[3].style.backgroundColor = "#2E95A7";
-    links[3].style.color = "#2E95A7";
-    links[3].style.opacity = 1;
+      bullets[3].style.backgroundColor = "#2E95A7";
+      links[3].style.color = "#2E95A7";
+      links[3].style.opacity = 1;
+    }
   }
+
 }
